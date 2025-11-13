23b474e8fecbbe060ff83049ba6dc01119ec8bb8
eu <acbjunqueira@usp.br>
2025-11-13 15:10:03 -0300

    teste

---
+++ .pio/build/frdm_kl25z/modules/picolibc/newlib/libm/common/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/junqu/.platformio/packages/framework-zephyr/_pio/modules/lib/picolibc/newlib/libm/common
+# Build directory: C:/Users/junqu/Documents/GitHub/atividade-comunica-o-2-arthur/.pio/build/frdm_kl25z/modules/picolibc/newlib/libm/common
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
